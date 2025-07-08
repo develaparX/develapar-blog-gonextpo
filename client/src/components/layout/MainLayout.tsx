99c727a1d31c98847e425ea38d8c91e35e245152
arfiansr <srarfian@gmail.com>
2025-07-08 21:08:57 +0700

    [ADD] client : navbar for main layout

--- client/src/components/layout/MainLayout.tsx
+++ client/src/components/layout/MainLayout.tsx
@@ -1,49 +1,57 @@
 import { Outlet, Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
+import { Bell, User } from "lucide-react";
 
 const MainLayout = () => {
+  const loggedIn = false;
+
   return (
     <div className="flex flex-col min-h-screen">
-      {/* Floating Navbar */}
-      <div className=" top-6 left-1/8 -translate-x-1/2 z-5 shadow-lg">
-        <img src={logo} alt="" width={150} className=" rounded-xl " />
-      </div>
+      {/* Navbar */}
+      <nav className="b w-full py-5">
+        <div className="flex justify-evenly items-center ">
+          <div className="flex gap-8 items-center">
+            <div className="max-w-60">
+              <img src={logo} width={"140"}></img>
+            </div>
 
-      <header className="fixed top-6 left-1/2 -translate-x-1/2 z-50">
-        <nav className="backdrop-blur-md bg-white/50 dark:bg-black/10 shadow-lg px-6 py-2 rounded-full flex gap-6 items-center text-xl font-normal border border-gray-100 dark:border-gray-300">
-          <Link to="/" className="hover:text-blue-600  transition">
-            Home
-          </Link>
-          <Link to="/about" className="hover:text-blue-600 transition">
-            About
-          </Link>
-          <Link to="/contact" className="hover:text-blue-600 transition">
-            Contact
-          </Link>
-          <Link to="/blog" className="hover:text-blue-600 transition">
-            Blog
-          </Link>
-        </nav>
-      </header>
+            <div className="flex gap-8 border-l-2">
+              <div className="pl-4">Stories</div>
+              <div>Idea</div>
+              <div>Gaming</div>
+              <div>Tech</div>
+            </div>
+          </div>
+          <div className="flex items-center gap-5">
+            {loggedIn ? (
+              <>
+                <button className="bg-gray-200 hover:bg-gray-700 hover:text-white rounded-lg px-5">
+                  Write here
+                </button>
 
-      <div className="fixed top-6 right-1/25 -translate-x-1/2 z-5">
-        <nav className="backdrop-blur-md bg-white/50 dark:bg-black/10 shadow-lg px-6 py-2 rounded-full flex gap-6 items-center text-xl font-normal border border-gray-100 dark:border-gray-300">
-          <Link to="/" className="hover:text-blue-600  transition">
-            login
-          </Link>
-          <Link to="/about" className="hover:text-blue-600 transition">
-            logout
-          </Link>
-        </nav>
-      </div>
+                <div>
+                  <Bell size={20} />
+                </div>
+                <div>
+                  <User size={20} />
+                </div>
+              </>
+            ) : (
+              <div className="border hover:bg-black hover:text-white  rounded-lg py-1 px-10">
+                Sign Up!
+              </div>
+            )}
+          </div>
+        </div>
+      </nav>
 
       {/* Main content */}
-      <main className="flex-1 pt-24">
+      <main className="flex-1">
         <Outlet />
       </main>
 
       {/* Footer */}
-      <footer className="bg-black text-white py-10 mt-20">
+      <footer className="bg-black text-white py-2 mt-20">
         <div className="max-w-6xl mx-auto px-4 text-center space-y-4">
           <p className="text-lg font-semibold">
             YourBlog © {new Date().getFullYear()}
