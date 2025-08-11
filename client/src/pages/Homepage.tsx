3bae9aa8329256558e25027d9dbd4a067af79ff4
arfiansr <srarfian@gmail.com>
2025-08-11 15:25:43 +0700

    feat : top hero homepage

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardAction,
@@ -6,71 +5,129 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { ArrowRight } from "lucide-react";
 
 const dummyData = [
   {
-    title: "Card Title 1",
-    description: "This is a description for card 1",
+    title: "lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    description:
+      "lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto corrupti impedit ipsam,",
     author: "Author 1",
+    imageUrl:
+      "https://images.unsplash.com/photo-1743449661678-c22cd73b338a?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    title: "Card Title 2",
-    description: "This is a description for card 1",
-    author: "Author 1",
+    title: "lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    description:
+      "lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto corrupti impedit ipsam,",
+    author: "Author 2",
+
+    imageUrl:
+      "https://images.unsplash.com/photo-1743449661678-c22cd73b338a?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
-    title: "Card Title 2",
-    description: "This is a description for card 1",
-    author: "Author 1",
+    title: "lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    description:
+      "lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto corrupti impedit ipsam,",
+    author: "Author 3",
+
+    imageUrl:
+      "https://images.unsplash.com/photo-1743449661678-c22cd73b338a?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
 const Homepage = () => {
   return (
     <div className="mx-auto px-5">
-      <div className="my-5">OH Hi! Welcome to develapar!</div>
+      <div className="my-10 flex justify-center items-center w-full text-4xl font-extrabold">
+        OH Hi! Welcome to develapar!
+      </div>
+
+      {/* TOP HERO */}
       <div
         id="top-menu"
         className="flex flex-row gap-5 justify-between w-full h-[40vw] items-center "
       >
-        <Card className=" w-full h-full max-w-[50%] bg-blue-300 rounded-4xl">
-          <div className="flex-1" />
+        {/* === KARTU KIRI YANG DIMODIFIKASI UNTUK REACT === */}
+        <Card
+          className="group relative w-full h-full max-w-[70%] rounded-4xl p-0 shadow-none overflow-hidden
+                     transition-all duration-300 ease-in-out "
+        >
+          <img
+            alt="Background"
+            src="https://images.unsplash.com/photo-1743449661678-c22cd73b338a?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" // Ganti
+            className="absolute inset-0 w-full h-full object-cover"
+          />
 
-          <CardHeader className="flex flex-row items-start justify-between">
-            <div className="flex flex-col gap-1">
-              <CardTitle>Ini Judul COntentnya</CardTitle>
-              <CardDescription>Ini Decsripsi conetenya apa</CardDescription>
-              <CardDescription>Authornya disini bro</CardDescription>
-            </div>
+          <div
+            className="absolute inset-0 bg-black 
+                       opacity-50 group-hover:opacity-0 
+                       transition-opacity duration-300 ease-in-out"
+          />
 
-            <CardAction className="bg-white">
-              <Button variant="ghost">Card Action</Button>
-            </CardAction>
-          </CardHeader>
+          <div className="relative flex flex-col h-full justify-end">
+            <CardHeader className="flex flex-row items-end justify-between p-0 text-white">
+              <div className="flex flex-col gap-1 p-5">
+                <CardTitle className="text-2xl font-bold">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                </CardTitle>
+                <CardDescription className="text-gray-300">
+                  Ini Decsripsi conetenya apa Lorem ipsum dolor sit, amet
+                  consectetur adipisicing elit. Iusto corrupti impedit ipsam,
+                </CardDescription>
+                <CardDescription className="text-gray-300">
+                  Authornya disini bro
+                </CardDescription>
+              </div>
+
+              <CardAction className="bg-white text-black min-w-[20%] h-auto flex items-center justify-center rounded-lg shadow-none m-5 p-4 hover:scale-110">
+                <ArrowRight size={80} />
+              </CardAction>
+            </CardHeader>
+          </div>
         </Card>
 
         {/* Kanan */}
-        <div className="w-full h-full max-w-[50%] flex flex-col gap-4 justify-between">
+        <div className="w-full h-full max-w-[35%] flex flex-col gap-5 justify-between">
           {dummyData.map((item, index) => (
             <Card
               key={index}
-              className=" py-2 shadow-none border-none  flex flex-row h-1/3"
+              className="py-2 shadow-none border-none flex flex-row h-1/3"
             >
-              <Card className=" bg-amber-700 w-[50%] h-[100%] rounded-4xl">
-                <p>Ini Isinya foto ntar</p>
+              <Card className="relative group w-[50%] h-[100%] rounded-xl overflow-hidden">
+                <img
+                  src={item.imageUrl}
+                  alt={item.title}
+                  className="absolute inset-0 w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
+                />
+
+                <div
+                  className="absolute inset-0 bg-gradient-to-t from-black/60 to-transparent 
+               opacity-70 group-hover:opacity-100 transition-opacity duration-300"
+                />
+
+                <CardAction
+                  className="absolute bottom-4 right-4 bg-white text-black rounded-full p-2 m-0
+              opacity-100 transition-all duration-300 ease-in-out
+               transform group-hover:scale-110"
+                >
+                  <ArrowRight size={24} />
+                </CardAction>
               </Card>
 
-              <CardHeader className="flex px-0 flex-col justify-evenly items-start  w-full">
+              <CardHeader className="flex px-0 flex-col justify-evenly items-start w-full">
                 <CardTitle className="pt-1">{item.title}</CardTitle>
-                <CardDescription>Ini Deskripsi Card Lainnya</CardDescription>
+                <CardDescription>{item.description}</CardDescription>
                 <CardDescription className="pb-1">
-                  Ini Deskripsi Card Lainnya
+                  By: {item.author}
                 </CardDescription>
               </CardHeader>
             </Card>
           ))}
         </div>
       </div>
+
+      {/* MIDDLE HERO */}
     </div>
   );
 };
